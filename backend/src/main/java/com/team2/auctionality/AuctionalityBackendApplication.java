462b9df07bc6f1eaa12ab7bff729f532b41cca54
Thuan Nguyen <nguyenquocthuan2905@gmail.com>
2025-11-29 00:38:41 +0700

    setup backend codebase

---
+++ backend/src/main/java/com/team2/auctionality/AuctionalityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team2.auctionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionalityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionalityBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/team2/auctionality/AuctionalityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.auctionality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionalityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
