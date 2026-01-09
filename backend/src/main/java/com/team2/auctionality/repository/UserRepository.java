b5c499685652a93715d59e55a53f6133fcabfd51
huy37204 <huy37204@gmail.com>
2026-01-09 17:45:16 +0700

    feat: fix user unknown in admin

--- backend/src/main/java/com/team2/auctionality/repository/UserRepository.java
+++ backend/src/main/java/com/team2/auctionality/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.team2.auctionality.repository;
 
 import com.team2.auctionality.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +13,15 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = {"profile", "roles"})
+    Page<User> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {
+            "profile",
+            "roles",
+            "socialLoginAccounts"
+    })
+    Optional<User> findById(Integer id);
 }
 
